e82806b2269adcb3ce736b68ac5d75918e5eac5b
Bogdan Mihaila <bomi@itu.dk>
2024-05-02 11:00:06 +0200

    yeepeeee

--- data_bogdan/extractor.py
+++ data_bogdan/extractor.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-# Load the data
-data = pd.read_csv(r'C:\Users\bogdan\Desktop\Uni\NLP\data_bogdan\Pokemon_names.txt', delimiter='\t')
-# print(data.head())
-# print(data.columns)
-# print(data["Englisch"].head())
+# Define the languages, corresponding CSV files, and column names
+languages = ["English", "German", "French", "Japanese"]
+files = ['data/manual/english/english_pokemon_names_cleaned.csv', 'data/manual/german/german_pokemon_names_cleaned.csv', 'data/manual/french/french_pokemon_names_cleaned.csv', 'data/manual/japanese/japanese_pokemon_names_cleaned.csv']
+column_names = ['Englisch', 'Deutsch', 'Französisch', 'Japanisch']
 
-data.columns = data.columns.str.strip()
+# Create an empty dictionary to hold the new data
+new_pokemon_data = {"Names": {lang: [] for lang in languages}}
 
-# Convert entire 'Deutsch' column to string and process to keep only alphabetical parts
-data['Japanisch'] = data['Japanisch'].astype(str).apply(lambda x: ' '.join([item for item in x.split() if item.isalpha()]))
+# Loop through the languages, corresponding CSV files, and column names
+for lang, file, column_name in zip(languages, files, column_names):
+    # Read the CSV file
+    df = pd.read_csv(file)
 
-# Save the cleaned names to a CSV file
-data[['Japanisch']].to_csv('japanese_pokemon_names_cleaned.csv', index=False)
+    # Loop through the rows in the CSV file
+    for index, row in df.iterrows():
+        # Add the data to the dictionary in the desired format
+        new_pokemon_data["Names"][lang].append(row[column_name])
 
-print("Cleaned Pokémon names have been saved to 'japanese_pokemon_names_cleaned.csv'.")
+print(new_pokemon_data)
